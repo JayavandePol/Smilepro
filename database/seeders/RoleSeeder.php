9cd1cf4a9c2f79497e60690b6c7cbb3c4e23c2d7
JayavandePol <341509@student.mboutrecht.nl>
2025-11-28 13:15:10 +0100

    first commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $roles = [
+            'patient',
+            'tandarts',
+            'mondhygienist',
+            'assistent',
+            'praktijkmanagement',
+        ];
+
+        foreach ($roles as $role) {
+            Role::firstOrCreate(['name' => $role]);
+        }
+    }
+}
